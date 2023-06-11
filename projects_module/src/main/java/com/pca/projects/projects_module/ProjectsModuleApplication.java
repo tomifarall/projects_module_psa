f539fe50fb16b6f1442a9f010b0ccb93dd8759c5
tomifarall <39673189+tomifarall@users.noreply.github.com>
2023-06-11 12:25:50 -0300

    Agrego esqueleto de la aplicacion

---
+++ projects_module/src/main/java/com/pca/projects/projects_module/ProjectsModuleApplication.java
@@ -0,0 +1,13 @@
+package com.pca.projects.projects_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsModuleApplication.class, args);
+	}
+
+}
---
+++ projects_module/src/test/java/com/pca/projects/projects_module/ProjectsModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pca.projects.projects_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
