4fa8faddf1b8b90bd9cad209154dfcaf9d95c36a
Julián <juliangorge@hotmail.com>
2023-06-18 19:24:06 -0300

    Agrego atributos según MDD

--- projects_module/src/main/java/com/pca/projects/projects_module/model/Project.java
+++ projects_module/src/main/java/com/pca/projects/projects_module/model/Project.java
@@ -6,10 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -21,5 +19,90 @@ public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long code;
+
+    private String name;
+
+    private String description;
+
+    private Integer status;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Double hoursWorked;
+
+    private Double tasksQuantity;
+
+    public Project(){
+    }
+
+    public Project(String name, String description, Integer status, Date startDate, Date endDate, Double hoursWorked, Double tasksQuantity){
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.hoursWorked = hoursWorked;
+        this.tasksQuantity = tasksQuantity;
+    }
+
+    public Long getCode(){
+        return this.code;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public Integer getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(Integer status){
+        this.status = status;
+    }
+
+    public Date getStartDate(){
+        return this.startDate;
+    }
+
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate(){
+        return this.endDate;
+    }
+    public void setEndDate(Date endDate){
+        this.endDate = endDate;
+    }
+
+    public Double getHoursWorked(){
+        return this.hoursWorked;
+    }
+    public void setHoursWorked(Double hoursWorked){
+        this.hoursWorked = hoursWorked;
+    }
+
+    public Double getTasksQuantity(){
+        return this.tasksQuantity;
+    }
+    public void setTasksQuantity(Double tasksQuantity){
+        this.tasksQuantity = tasksQuantity;
+    }
+
 }
--- projects_module/src/main/java/com/pca/projects/projects_module/model/ProjectTask.java
+++ projects_module/src/main/java/com/pca/projects/projects_module/model/ProjectTask.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -22,4 +23,83 @@ public class ProjectTask {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String title;
+    private String description;
+    private Integer type_id;
+    private Integer priority;
+    private Integer status;
+    private Date startDate;
+    private Date endDate;
+    private Double estimatedTime;
+    private Double timeWorked;
+
+    public ProjectTask(){
+    }
+
+    public ProjectTask(String title, String description, Integer type_id, Integer priority, Integer status, Date startDate, Date endDate, Double estimatedTime){
+        this.title = title;
+        this.description = description;
+        this.type_id = type_id;
+        this.priority = priority;
+        this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.estimatedTime = estimatedTime;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public Integer getTypeId(){
+        return this.type_id;
+    }
+    public void setTypeId(Integer type_id){
+        this.type_id = type_id;
+    }
+    public Integer getPriority(){
+        return this.priority;
+    }
+    public void setPriority(Integer priority){
+        this.priority = priority;
+    }
+    public Integer getStatus(){
+        return this.status;
+    }
+    public void setStatus(Integer status){
+        this.status = status;
+    }
+    public Date getStartDate(){
+        return this.startDate;
+    }
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+    public Date getEndDate(){
+        return this.endDate;
+    }
+    public void setEndDate(Date endDate){
+        this.endDate = endDate;
+    }
+    public Double getEstimatedTime(){
+        return this.estimatedTime;
+    }
+    public void setEstimatedTime(Double estimatedTime){
+        this.estimatedTime = estimatedTime;
+    }
+    public Double getTimeWorked(){
+        return this.timeWorked;
+    }
+    public void setTimeWorked(Double timeWorked){
+        this.timeWorked = timeWorked;
+    }
 }
