cb2cca17f91077877a598dabfb5f63e801005219
tomifarall <tom.i7@hotmail.com>
2023-06-26 23:17:06 -0300

    Prueba con cors config

--- src/main/java/com/pca/projects/projects_module/controller/ProjectTaskController.java
+++ src/main/java/com/pca/projects/projects_module/controller/ProjectTaskController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Api(tags = "Project Task Controller")
 public class ProjectTaskController {
 
